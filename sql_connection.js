d0a83057e18269ec7f2d323c434232e034cb2db5
张志哲 <lol>
2020-08-23 22:51:09 +0800

    Updated Grammar

--- sql_connection.js
+++ sql_connection.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if (err) { throw err }
-    console.log('MySql Connected...')
+    console.log('MySQL Connected...')
 })
 
 module.exports = db
